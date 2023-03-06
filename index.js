f39e52d7f7a0ab97947172abeacb6151f86a6a6b
Christoph Obst <christoph.obst@posteo.de>
2023-03-06 11:00:59 +0100

    add card component

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
+import { createCharacterCard } from "./components/card/card.js";
+
 const cardContainer = document.querySelector('[data-js="card-container"]');
 const searchBarContainer = document.querySelector(
   '[data-js="search-bar-container"]'
 );
+
 const searchBar = document.querySelector('[data-js="search-bar"]');
 const navigation = document.querySelector('[data-js="navigation"]');
 const prevButton = document.querySelector('[data-js="button-prev"]');
 const nextButton = document.querySelector('[data-js="button-next"]');
 const pagination = document.querySelector('[data-js="pagination"]');
 
-// States
+
+//states
 const maxPage = 1;
 const page = 1;
 const searchQuery = "";
